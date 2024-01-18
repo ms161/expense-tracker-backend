9a0a6747979515d20586259db263eaddbfe0c0ec
ms161 <msehrawat161@gmail.com>
2024-01-19 02:06:42 +0530

    task 5

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,6 @@ exports.signUp = async (req, res) => {
         const name = req.body.name
         const email = req.body.email
         const password = req.body.password
-
         const user = await User.findAll({ where: { email: email } })
 
         if (user.length > 0) {
@@ -22,5 +21,29 @@ exports.signUp = async (req, res) => {
     }
     catch (err) {
         console.log(err)
+    }
+}
+
+exports.login = async (req, res) => {
+try
+    {const email = req.body.email
+    const password = req.body.password
+
+    const user = await User.findAll({ where: { email: email } })
+    if (user.length > 0) {
+        const userPassword = user[0].dataValues.password
+        if (password === userPassword) {
+            return res.status(200).json({ message: 'User login sucessful' })
+        }
+        else {
+            return res.status(401).json({ message: 'User not authorized' })
+        }
+
+    }
+    else {
+        res.status(404).json({ message: 'User not found' })
+    }}
+    catch(err){
+    res.status(500).json({error:err})
     }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,5 +5,7 @@ const userController=require('../controllers/userController')
 
 router.post('/sign-up',userController.signUp)
 
+router.post('/login',userController.login) 
 
 module.exports=router
+
