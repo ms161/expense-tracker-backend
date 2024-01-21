74663c22ba3e2a19f42d1ba7b2093a6708daaba8
ms161 <msehrawat161@gmail.com>
2024-01-22 03:46:17 +0530

    task 13

--- controllers/expenseControllers.js
+++ controllers/expenseControllers.js
@@ -1,46 +1,56 @@
+const sequelize = require('../util/database');
+const User=require('../models/user')
 const Expense = require('../models/userExpense')
 
-function Invalidstring(str){
-    if(str.trim().length==0 || str == undefined){
+function Invalidstring(str) {
+    if (str.trim().length == 0 || str == undefined) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
 exports.postExpense = async (req, res) => {
+   const t = await sequelize.transaction()
     try {
+       
+       
         const { amount, description, category } = req.body
-        console.log('this is id',req.user)
-        if(Invalidstring(amount) || Invalidstring(description) || Invalidstring(category)){
-            return res.status(400).json({message:'All the fields are mandatory'})
+        console.log('this is id', req.user)
+        if (Invalidstring(amount) || Invalidstring(description) || Invalidstring(category)) {
+            return res.status(400).json({ message: 'All the fields are mandatory' })
         }
         const resp = await Expense.create({
             amount,
             description,
             category,
-            userId:req.user.id 
-        
-        })
+            userId: req.user.id,
+
+
+        }, { transaction: t })
 
         //MAGIC ASSOCIATION 
         // const resp=req.user.createExpense({amount,
         //     description,
         //     category,
         // })
-        const totalExpense = Number(req.user.totalExpense)+ Number(amount);
-        console.log('amount',totalExpense);
+        const totalExpense = Number(req.user.totalExpense) + Number(amount);
+        console.log('amount', totalExpense);
         await User.update({
             totalExpense: totalExpense
-        },{
-            where:{id: req.user.id},
-        
-        })
+        }, {
+            where: { id: req.user.id },
+            transaction: t
 
+        })
+        await t.commit()
         res.status(201).json(resp)
 
     }
     catch (err) {
+     
+            await t.rollback();
+      
         console.log(err)
         res.json({ error: err })
     }
@@ -49,7 +59,7 @@ exports.postExpense = async (req, res) => {
 
 exports.getAllExpenses = async (req, res) => {
     try {
-        const expenses = await Expense.findAll({where:{userId:req.user.id}})
+        const expenses = await Expense.findAll({ where: { userId: req.user.id } })
         //MAGIC ASSOCIATION METHOD
         // const expenses=await req.user.getExpense()
         console.log('this is expenses', expenses)
@@ -63,15 +73,29 @@ exports.getAllExpenses = async (req, res) => {
 }
 
 exports.deleteExpense = async (req, res) => {
+    const t = await sequelize.transaction(); 
     try {
         const id = req.params.id
-        console.log(id)
-        const resp = await Expense.destroy({ where: { id: id,userId:req.user.id } })
+     
+        const price = await Expense.findAll({where:{id:id}});
+
+        const resp = await Expense.destroy({ where: { id: id, userId: req.user.id } })
+        const totalExpense = Number(req.user.totalExpense) - Number(price[0].amount);
+
+        await User.update({
+            totalExpense: totalExpense
+        },{
+            where:{id: req.user.id},
+            transaction : t
+        })
+
+        await t.commit();
         res.status(200).json(resp)
     }
     catch (err) {
+        await t.rollback();
         console.log(err)
 
-        res.json()
+        res.status(500).json({message:err});
     }
 }
