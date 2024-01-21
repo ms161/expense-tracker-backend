93e5a91b6e21b2635eb182251dc7458b2c78be37
ms161 <msehrawat161@gmail.com>
2024-01-22 02:41:30 +0530

    task 11

--- controllers/premiumController.js
+++ controllers/premiumController.js
@@ -1,27 +1,22 @@
+const sequelize = require('sequelize')
 const User = require('../models/user')
 const Expense = require('../models/userExpense')
 
 exports.showLeaderboard = async (req, res) => {
-    const user = await User.findAll()
-    const expense = await Expense.findAll()
-
-    const obj = {}
-    expense.forEach((e) => {
-        if (obj[e.userId]) {
-            obj[e.userId] += e.amount
-        }
-        else {
-            obj[e.userId] = e.amount
-        }
+    const userLeaderboard = await User.findAll({
+        attributes:['id','name',[sequelize.fn('sum',sequelize.col('expenses.amount')),'total_cost']], 
+        include:[
+            {
+                model:Expense, 
+                attributes:[]
+            }
+        ],
+        group: ['user.id'],
+        order:[[sequelize.col('total_cost'),'DESC']]
     })
-let userLeaderBoard=[]
-    user.forEach((e)=>{
-        userLeaderBoard.push({name:e.name,total_cost:obj[e.id]})
+    
+   
 
-    })
-    console.log(userLeaderBoard)
-    // console.log(obj)
-
-    res.json(user)
+    res.json(userLeaderboard)
 
 }
